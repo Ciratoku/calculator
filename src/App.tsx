36ac1070dd588a112e8457b1bb7063c9c32b0c61
Ciratoku <edissonman255@gmail.com>
2024-08-05 20:51:00 +0300

    frontend done?

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
 import { useState } from "react";
 import "./App.css";
+import "./style.css";
 
 function App() {
-  return <></>;
+  const buttons = [
+    "C",
+    "√",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "00",
+    "0",
+    ",",
+  ];
+  return (
+    <div className="calc-grid">
+      <div className="output">
+        <div className="statement">123</div>
+        <div className="result">123</div>
+      </div>
+      {buttons.map((btn) => (
+        <button key={btn}>{btn}</button>
+      ))}
+      <button style={{ color: "rgba(0, 0, 0, 0.5)", background: "white" }}>
+        =
+      </button>
+    </div>
+  );
 }
 
 export default App;
