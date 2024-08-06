0bc67cccba15ae633dc8ef0519cb1271544c741d
Ciratoku <edissonman255@gmail.com>
2024-08-06 20:22:08 +0300

    evaluate in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
+import calculate from "./util/evaluate";
 
 function App() {
   const buttons = [
@@ -10,7 +11,7 @@ function App() {
     "7",
     "8",
     "9",
-    "x",
+    "*",
     "4",
     "5",
     "6",
@@ -21,22 +22,59 @@ function App() {
     "+",
     "00",
     "0",
-    ",",
+    ".",
   ];
-  const handleInput = (e: Event) => {
-    //
-  };
+  const [calculated, setCalculated] = useState(false);
+  const [statement, setStatement] = useState("");
+  const [result, setResult] = useState("");
+  const handleKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      const key = event.key;
+      if (key == "Escape" || key == "C") {
+        setResult(() => "");
+        setStatement(() => "");
+      } else if (key == "Enter" || key == "=") {
+        let ans = calculate(result);
+        if (Number.isNaN(ans)) ans = "Error!";
+        setStatement(result);
+        setResult(ans.toString());
+        setCalculated(true);
+      } else if (buttons.includes(key) || key == "(" || key == ")") {
+        if (calculated) {
+          setStatement("");
+          setResult("");
+          setCalculated(false);
+        }
+        setResult((prev) => prev + key);
+      }
+    },
+    [result, statement]
+  );
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp]);
   return (
     <div className="wrapper">
       <div className="calc-grid">
         <div className="output">
-          <div className="statement">123</div>
-          <div className="result">123</div>
+          <div className="statement">{statement}</div>
+          <div className="result">{result}</div>
         </div>
         {buttons.map((btn) => (
-          <button key={btn}>{btn}</button>
+          <button
+            key={btn}
+            onClick={() =>
+              handleKeyUp(new KeyboardEvent("keyup", { key: btn }))
+            }
+          >
+            {btn}
+          </button>
         ))}
-        <button style={{ color: "rgba(0, 0, 0, 0.5)", background: "white" }}>
+        <button
+          style={{ color: "#2a5698", background: "white" }}
+          onClick={() => handleKeyUp(new KeyboardEvent("keyup", { key: "=" }))}
+        >
           =
         </button>
       </div>
